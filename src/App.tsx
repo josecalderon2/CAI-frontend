75e94f28b9961e464ef1419a12069223ebfe1171
RodolfoRivasRodriguez <rodolfo.rivas@catolica.edu.sv>
2025-11-11 14:49:31 -0800

    Revert "Refactor sistema evaluacion"
    This reverts commit a7ff5ed7262466970614fd4d51cfbba71533b80c.

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ import { PerfilModule } from './components/PerfilModule';
 import { AsistenciaModuleNew } from './components/AsistenciaModuleNew';
 import { ConductaModule } from './components/ConductaModule';
 import { ReportesModule } from './components/ReportesModule';
-import NotasModuleWrapper from './components/NotasModuleWrapper';
+import NotasModule from './components/NotasIngresoMensualCurso';
 
 // ================= Helpers de auth =================
 function getUser() {
@@ -397,7 +397,7 @@ function AppRoutes() {
           path="/notas"
           element={
             <ProtectedRoute>
-              <NotasModuleWrapper />
+              <NotasModule />
             </ProtectedRoute>
           }
         />
